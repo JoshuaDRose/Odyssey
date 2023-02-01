fd3428cbfceb90366459a043b0310edc206a7588
= <joshuarose099@gmail.com>
2023-02-01 13:37:23 +1100

    Added logger to file constructor

--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -1,5 +1,6 @@
 from .core import get_folder, get_parent, get_size
 from .core import get_insert_paths
 from .camera import Background, Camera
+from .logger import Logger
 from .debug import TextBox
 from .spritesheet import Spritesheet
