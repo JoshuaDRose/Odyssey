734ede41b3c16d94502880de7c98bedd4498dc43
= <joshuarose099@gmail.com>
2023-02-01 13:09:27 +1100

    Added debug stuff

--- src/utils/debug.py
+++ src/utils/debug.py
@@ -1,1 +1,17 @@
 """ Used for debugging, as name suggests :) """
+import utils
+import pygame
+
+
+
+class Textbox(pygame.sprite.Sprite):
+    fontPath = pygame.font.match_font('InconsolataLGC Nerd Font')
+
+    def __init__(self, x, y):
+        pygame.font.init()
+        
+
+
+def text():
+    """ Display text in a small debug window """
+    raise NotImplementedError
