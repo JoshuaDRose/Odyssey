2469ead3a06c36849df7cb99d52999f951f551e0
= <joshuarose099@gmail.com>
2023-02-04 20:36:31 +1100

    Added button class methods and constructor

--- src/utils/ui/button.py
+++ src/utils/ui/button.py
@@ -14,6 +14,15 @@ class Choice(pygame.sprite.Sprite):
 
 class Button(pygame.sprite.Sprite):
     """ Button, can be used in choice class as well """
-    def __init__(self, x, y, group):
+    def __init__(self, x, y, image, group):
+        """ Constructor inherits all sprite methods, draw is overriden unless
+            called from an instance of pygame.sprite.Group().draw()
+        """
         super().__init__(group)
-        self.image = 
+        self.image = image
+        self.rect = self.image.get_rect()
+        self.rect.x, self.rect.y = x, y
+
+    def draw(self, surface) -> None:
+        """ Draw image to surface at position of rect """
+        surface.blit(self.image, self.rect)
