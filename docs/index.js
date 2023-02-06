b76802263f05009c0f412fbf71fb7edbb787652a
= <joshuarose099@gmail.com>
2023-02-06 12:50:16 +1100

    Added js and php

--- docs/index.js
+++ docs/index.js
@@ -1,13 +1,17 @@
-var a = 1, b = 2;
-var div = document.createElement('div');
-div.setAttribute('class', 'post block bc2');
-div.innerHTML = `
-    <div class="parent">
-    <div class="child">${a}</div>
-    <div class="child">+</div>
-    <div class="child">${b}</div>
-    <div class="child">=</div>
-    <div class="child">${a + b}</div>
-    </div>
-    `;
+var xmlhttp = new XMLHttpRequest();
+var url = "core.php";
+
+xmlhttp.onreadystatechange = function () {
+    if (this.readyState == 4 && this.status == 200) {
+        getDbData(this.responseText);
+    }
+}
+xmlhttp.open("GET", url, true);
+xmlhttp.send();
+
+
+function getDbData(response) {
+    console.log(response);
+}
+
 document.getElementById('posts').appendChild(div);
