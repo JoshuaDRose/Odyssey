4bd019fa0011d7d63933df5290da9140b400ac87
= <joshuarose099@gmail.com>
2023-02-06 12:42:30 +1100

    Format

---
+++ docs/index.js
@@ -0,0 +1,13 @@
+var a = 1, b = 2;
+var div = document.createElement('div');
+div.setAttribute('class', 'post block bc2');
+div.innerHTML = `
+    <div class="parent">
+    <div class="child">${a}</div>
+    <div class="child">+</div>
+    <div class="child">${b}</div>
+    <div class="child">=</div>
+    <div class="child">${a + b}</div>
+    </div>
+    `;
+document.getElementById('posts').appendChild(div);
